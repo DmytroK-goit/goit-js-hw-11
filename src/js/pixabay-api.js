e7109f8143f1f6536f75e43b7633f9505df69eec
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-07-15 14:56:29 +0300

    repair

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,11 +3,11 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const loader = document.querySelector('.loader');
 
-function showLoader() {
+export function showLoader() {
   loader.style.display = 'block';
 }
 
-function hideLoader() {
+export function hideLoader() {
   loader.style.display = 'none';
 }
 
@@ -16,42 +16,5 @@ export function performSearch(searchQuery) {
   const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
     searchQuery
   )}&image_type=photo`;
-
-  showLoader();
-
-  return fetch(URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Не вдалося виконати запит.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      hideLoader();
-      if (data.totalHits > 0) {
-        iziToast.show({
-          position: 'topRight',
-          backgroundColor: 'green',
-          message: `Found ${data.totalHits} results.`,
-        });
-        return data.hits;
-      } else {
-        iziToast.show({
-          position: 'topRight',
-          backgroundColor: 'red',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        return [];
-      }
-    })
-    .catch(error => {
-      hideLoader();
-      iziToast.show({
-        position: 'topRight',
-        backgroundColor: 'red',
-        message: 'Error during the request. Please try again later.',
-      });
-      throw error;
-    });
+  return fetch(URL);
 }
